f45afbebabe49f6131dc34a27c344f6c3c6fb83c
Martinez <write2martinez@gmail.com>
2023-10-06 13:43:01 -0700

    Deciding whether a number returns negative, positive, or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,29 +2,22 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Determine if a random number is positive, negative or zero.
-(*
- * Return: 0 on success
- */
+ *main - the main function
+ *
+ *Return: 0 on success
+ /*
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n < 0)
-	{
-		printf("%d is %s\n", n, "negative");
-	}
-	else if (n > 0)
-	{
-		printf("%d is %s\n", n, "positive");
-	}
-	else
-	{
-		printf("%d is %s\n", n, "zero");
-	}
+	if (n > 0)
+	  printf("%d is positive", n);
+	else if (n = 0)
+	  printf("%d is zero", n);
+	else if (n < 0)
+	  printf("%d is negative", n);
 	return (0);
-
 }
